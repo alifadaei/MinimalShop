fd0209c2daf7043a8554da58f2725a8d0f4953b4
Ali <alifadaeimanesh@gmail.com>
2022-06-20 12:06:16 +0430

    make banner component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Header from "./components/header";
-
+import Banner from "./components/banner";
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Banner />
+      </main>
     </React.Fragment>
   );
 }
